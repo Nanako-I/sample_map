9f8cfdb14864c17721ca998b01607bffbe2ecbe5
Nanako-I <milklmi.1103775@gmail.com>
2022-11-30 23:11:29 +0900

    html追加

--- js/map.js
+++ js/map.js
@@ -8,24 +8,86 @@
       //lat=緯度、lon=経度 を取得
       const lat = position.coords.latitude;
       const lon = position.coords.longitude;
+      let map=new Microsoft.Maps.Map("#myMap",{
+        center:new Microsoft.Maps.Location(35.544183970713334, 134.81330869397715)
+      });
 
-      //Map表示
-      map = new Bmap("#myMap");
-      map.startMap(35.544183970713334, 134.81330869397715, "load", 20); //The place is Bellevue.
-      //Pinを追加
-      let pin = map.pin(35.544183970713334, 134.81330869397715, "#0066FF");
-      let pin_izushi_tokubetu = map.pin(35.47786214757359, 134.86872629492163, "#ff9900");
-      let pin_seiwaen = map.pin(35.4765884892285, 134.86928487545512, "#ff0000");
-      let pin_seiwaen2 = map.pin(35.4714349,134.8419697, "#ff0000");
-      let pin_toyosyou = map.pin(35.541965219092525, 134.82205814002293, "#ff9900");
-      //Infoboxを追加
-      map.infobox(35.544183970713334, 134.81330869397715, "豊岡駅", "駅");
-      map.infobox(35.47786214757359, 134.86872629492163, "出石特別支援学校", "特別支援学校");
-      map.infobox(35.4765884892285, 134.86928487545512, "出石精和園児童寮", "生活支援施設");
-      map.infobox(35.4714349,134.8419697, "出石精和園成人寮", "生活支援施設");
-      map.infobox(35.541965219092525, 134.82205814002293, "豊岡小学校", "ひまわり学級(特別支援学級)");
+      // centerからスタートさせる↓
+        let pushpin_station= new Microsoft.Maps.Pushpin(map.getCenter());
+        // pushpinを表示させる↓
+map.entities.push(pushpin_station);
+
+// -------------------------------------
+// 　　　　　豊小のpushpin↓
+// --------------------------------------
+let pushpin_toyosyou= new Microsoft.Maps.Pushpin({latitude:35.541965219092525, longitude:134.82205814002293},{color:"#ff9900",}  );
+// pushpinを表示させる↓
+map.entities.push(pushpin_toyosyou);
+
+// 豊小のpushpinクリックイベント↓
+ Microsoft.Maps.Events.addHandler(pushpin_toyosyou,"click",function(){
+  alert("click");
+  // $("#toyosyou").hide();
+  // toggleを使うことで、クリックしたら表示＆隠すことができる↓
+  $(".toyosyou").toggle(1000);
+            });
+// --------------------------------------
+// 　　　　　出石特別支援のpushpin↓
+// --------------------------------------
+let pushpin_izushi_school= new Microsoft.Maps.Pushpin({latitude:35.47786214757359, longitude:134.86872629492163},{color:"#ff9900",}  );
+// pushpinを表示させる↓
+map.entities.push(pushpin_izushi_school);
+
+// 豊小のpushpinクリックイベント↓
+ Microsoft.Maps.Events.addHandler(pushpin_izushi_school,"click",function(){
+  alert("click");
+  $(".izushi_tokubetsu").toggle(1000);
+            });
+
+// --------------------------------------
+// 　　　　　精和園成人寮のpushpin↓
+// --------------------------------------
+let pushpin_seiwaen= new Microsoft.Maps.Pushpin({latitude:35.4714349, longitude:134.8419697},{color:"#ff0000",}  );
+// pushpinを表示させる↓
+map.entities.push(pushpin_seiwaen);
+
+// 豊小のpushpinクリックイベント↓
+ Microsoft.Maps.Events.addHandler(pushpin_seiwaen,"click",function(){
+  alert("click");
+  $(".seiwaen").toggle(1000);
+            });
     };
 
+
+
+  
+
+
+      
+      //Map表示
+      // map = new Bmap("#myMap");
+      // map.startMap(35.544183970713334, 134.81330869397715, "load", 20); 
+      //The place is Bellevue.
+      //Pinを追加
+      // let pin = map.pin(35.544183970713334, 134.81330869397715, "#0066FF");
+      // let pin_izushi_tokubetu = map.pin(35.47786214757359, 134.86872629492163, "#ff9900");
+      // let pin_seiwaen = map.pin(35.4765884892285, 134.86928487545512, "#ff0000");
+      // let pin_seiwaen2 = map.pin(35.4714349,134.8419697, "#ff0000");
+      // let pin_toyosyou = map.pin(35.541965219092525, 134.82205814002293, "#ff9900");
+      //Infoboxを追加
+      // map.infobox(35.544183970713334, 134.81330869397715, "豊岡駅", "駅");
+      // map.infobox(35.47786214757359, 134.86872629492163, "出石特別支援学校", "特別支援学校");
+      // map.infobox(35.4765884892285, 134.86928487545512, "出石精和園児童寮", "生活支援施設");
+      // map.infobox(35.4714349,134.8419697, "出石精和園成人寮", "生活支援施設");
+      // map.infobox(35.541965219092525, 134.82205814002293, "豊岡小学校", "ひまわり学級(特別支援学級)");
+
+      // map.Events.addHandler(pin_toyosyou,"click",function(){
+      //   alert("click");
+      //       });
+
+    // };
+
+   
     //****************************************
     //失敗関数
     //****************************************
@@ -57,3 +119,9 @@
     function GetMap() {
       navigator.geolocation.getCurrentPosition(mapsInit, mapsError, set);
     }
+// $("pin_toyosyou").on("click",function(){
+// alert(click);
+// });
+// map.Events.addHandler(pin_toyosyou,"click",function(){
+// alert(click);
+    // });
